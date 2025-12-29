b132fd688f09a4307ff2bee985c09c34d09807c6
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-12-29 15:45:40 +0700

    fix dashboard

--- apps/client/api/booking.api.ts
+++ apps/client/api/booking.api.ts
@@ -16,3 +16,14 @@ export const getInviteLink = async (bookingId: string) => {
     const res = await axios.get(`${API_URL}/booking/${bookingId}/invite`);
     return res.data;
 };
+
+export const getBookingsForLearner = async (learnerId: string) => {
+    const res = await axios.get(`${API_URL}/booking/learner/${learnerId}`)
+    return res.data
+}
+
+export const getBookingsForSherpa = async (sherpaId: string) => {
+    const res = await axios.get(`${API_URL}/booking/sherpa/${sherpaId}`)
+    return res.data
+}
+
